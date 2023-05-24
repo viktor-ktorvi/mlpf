b38bd89b0565d7ec3a605280ded934b1c8002089
viktor-ltorvi <todosijevicviktor998@gmail.com>
2023-05-24 11:23:24 +0200

    add max_samples and shuffle options in load_data

--- mlpf/data/loading/load_data.py
+++ mlpf/data/loading/load_data.py
@@ -2,6 +2,7 @@ import collections
 import glob
 import os
 import pickle
+import random
 
 from pandapower import from_json
 from tqdm import tqdm
@@ -25,29 +26,49 @@ def load_pickle_ppc(filepath: str) -> Dict:
     return data_sample
 
 
-def load_data(path: str, extension: str = ".p", load_sample_function: Callable = load_pickle_ppc) -> List[Any]:
+def load_data(path: str,
+              extension: str = ".p",
+              load_sample_function: Callable = load_pickle_ppc,
+              max_samples=None,
+              shuffle=False) -> List[Any]:
     """
     Load all the data files with the given extension in the directory described by path. By default, assumes that the files are pickled.
 
+    :param shuffle:
     :param path: Path to the data directory.
-    :param extension: Pile extension.
+    :param extension: File extension.
     :param load_sample_function: Function to load the data samples. By default, is a function that loads a pickled object. The user can pass
     in their own function which takes a single string filepath argument.
+    :param max_samples: Maximum number of files to load if specified.
+    :param shuffle: Shuffle the filepaths if True.
     :return: List of the loaded data samples.
     """
+
+    filepaths = glob.glob(os.path.join(path, "*" + extension))
+
+    if shuffle:
+        random.shuffle(filepaths)
+
+    if max_samples is not None and len(filepaths) > max_samples:
+        filepaths = filepaths[:max_samples]
+
     data_list = []
-    for filepath in tqdm(glob.glob(os.path.join(path, "*" + extension)), ascii=True, desc="Loading files"):
+    for filepath in tqdm(filepaths, ascii=True, desc="Loading files"):
         data_list.append(load_sample_function(filepath))
 
     return data_list
 
 
-def autodetect_load_ppc(path: str) -> List[Dict]:
+def autodetect_load_ppc(path: str,
+                        max_samples=None,
+                        shuffle=False) -> List[Dict]:
     """
     Automatically detect the extension that appears in the given directory the most and load the files with that extension if supported.
     For more control over loading data use _load_data_.
 
     :param path: Data directory path.
+    :param max_samples: Maximum number of files to load if specified.
+    :param shuffle: Shuffle the filepaths if True.
     :return: List of PPCs
     """
     extension_counter = collections.Counter()
@@ -58,11 +79,17 @@ def autodetect_load_ppc(path: str) -> List[Dict]:
     most_common_extension = max(extension_counter, key=extension_counter.get)
 
     if most_common_extension == ".p":
-        return load_data(path, extension=".p", load_sample_function=load_pickle_ppc)
+        return load_data(path, extension=".p",
+                         load_sample_function=load_pickle_ppc,
+                         max_samples=max_samples,
+                         shuffle=shuffle)
 
     # TODO this could be decompositioned into it's own function.
     if most_common_extension == ".json":
-        pandapower_networks = load_data(path, extension=".json", load_sample_function=from_json)
+        pandapower_networks = load_data(path, extension=".json",
+                                        load_sample_function=from_json,
+                                        max_samples=max_samples,
+                                        shuffle=shuffle)
 
         return pandapower2ppc_list(pandapower_networks)
 

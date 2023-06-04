0f037c26b6a88a9e38833163cc005f6ef4f40b37
viktor-ltorvi <todosijevicviktor998@gmail.com>
2023-06-04 14:14:24 +0200

    add dtype argument to ppc2power_flow_arrays

--- mlpf/data/utils/conversion.py
+++ mlpf/data/utils/conversion.py
@@ -13,23 +13,24 @@ from torch import LongTensor, Tensor
 from mlpf.enumerations.bus_table import BusTableIds
 
 
-def ppc2power_flow_arrays(ppc: Dict) -> Tuple[ndarray, ndarray, ndarray, ndarray, ndarray, ndarray, ndarray]:
+def ppc2power_flow_arrays(ppc: Dict, dtype: np.dtype = np.float64) -> Tuple[ndarray, ...]:
     """
     Extract the physical values(needed for power flow) from a network
 
-    :param ppc: pypower case format dict
+    :param ppc: PyPower case format dict.
+    :param dtype: NumPy data type.
     :return: (edge_index, active_powers_pu, reactive_powers_pu, voltages_pu, angles_rad, conductances_pu, susceptances_pu)
     """
     # extract powers - convert to per unit and return generation minus demand
     complex_power = makeSbus(ppc['baseMVA'], ppc['bus'], ppc['gen'])
 
-    active_powers_pu = np.real(complex_power)
-    reactive_powers_pu = np.imag(complex_power)
+    active_powers_pu = np.real(complex_power).astype(dtype)
+    reactive_powers_pu = np.imag(complex_power).astype(dtype)
 
     # extract voltages and angles
-    voltages_pu = ppc['bus'][:, BusTableIds.voltage_magnitude_pu]
+    voltages_pu = ppc['bus'][:, BusTableIds.voltage_magnitude_pu].astype(dtype)
     angles_deg = ppc['bus'][:, BusTableIds.voltage_angle_deg]
-    angles_rad = np.deg2rad(angles_deg)
+    angles_rad = np.deg2rad(angles_deg).astype(dtype)
 
     warnings.filterwarnings("ignore", message="Casting complex values to real discards the imaginary part")
 
@@ -39,10 +40,10 @@ def ppc2power_flow_arrays(ppc: Dict) -> Tuple[ndarray, ndarray, ndarray, ndarray
     source, target = Y_sparse_matrix.nonzero()
     line_admittances = np.array(Y_sparse_matrix[source, target]).squeeze()
 
-    conductances_pu = np.real(line_admittances)
-    susceptances_pu = np.imag(line_admittances)
+    conductances_pu = np.real(line_admittances).astype(dtype)
+    susceptances_pu = np.imag(line_admittances).astype(dtype)
 
-    edge_index = np.vstack((source, target))
+    edge_index = np.vstack((source, target)).astype(np.int64)
 
     return edge_index, active_powers_pu, reactive_powers_pu, voltages_pu, angles_rad, conductances_pu, susceptances_pu
 

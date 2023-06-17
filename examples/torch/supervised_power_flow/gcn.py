be9caccbf384b825da8e35f3296393073fda63c7
viktor-ktorvi <todosijevicviktor998@gmail.com>
2023-06-17 13:02:10 +0200

    introduce metrics to gcn

--- examples/torch/supervised_power_flow/gcn.py
+++ examples/torch/supervised_power_flow/gcn.py
@@ -1,13 +1,15 @@
 import torch
 import torch.nn as nn
 import torch_geometric as pyg
+from pandas.io.json._normalize import nested_to_record
 from sklearn.model_selection import train_test_split
 from torch_geometric.loader import DataLoader
+from torchmetrics import MetricCollection, MeanSquaredError, R2Score
 from tqdm import tqdm
 
-from examples.torch.supervised_power_flow.utils import supervised_pf_logs, logs2str
 from mlpf.data.data.torch.power_flow import power_flow_data
 from mlpf.data.loading.load_data import autodetect_load_ppc
+from mlpf.loss.torch.metrics.power_flow import RelativePowerFlowError
 from mlpf.utils.standard_scaler import StandardScaler
 
 
@@ -44,7 +46,7 @@ def main():
 
     # Load ppcs and ppc -> Data
 
-    solved_ppc_list = autodetect_load_ppc("generated_ppcs", shuffle=True, max_samples=5000)
+    solved_ppc_list = autodetect_load_ppc("generated_ppcs", shuffle=True, max_samples=1000)
 
     pf_data_list = []
     for solved_ppc in tqdm(solved_ppc_list, ascii=True, desc="Converting ppcs to data"):
@@ -58,15 +60,13 @@ def main():
     # Torch dataloaders
 
     train_loader = DataLoader(data_train, batch_size=batch_size, shuffle=True)
-
     val_loader = DataLoader(data_val, batch_size=batch_size, shuffle=False)
 
     node_features_stacked = torch.vstack([data.x for data in data_train])
     train_targets = torch.vstack([data.target_vector for data in data_train])
 
-    # the output variables are of varying orders of magnitude so not normalizing them will the MSE loss
-    # favoring larger variables and the weights in the final layer being of varying orders of magnitude themselves.
-    # when calculating the power flow loss, an inverse transform needs to be applied to the predictions
+    output_size = train_targets.shape[1]
+
     output_scaler = StandardScaler(train_targets)
     output_scaler.to(device)
 
@@ -83,11 +83,21 @@ def main():
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     criterion = nn.MSELoss()
 
+    metrics_train = MetricCollection(
+        MeanSquaredError(),
+        R2Score(num_outputs=output_size),
+        RelativePowerFlowError(active_str="rel P", reactive_str="rel Q")
+    ).to(device)
+
+    metrics_val = MetricCollection(
+        MeanSquaredError(),
+        R2Score(num_outputs=output_size),
+        RelativePowerFlowError(active_str="rel P", reactive_str="rel Q")
+    ).to(device)
+
     progress_bar = tqdm(range(num_epochs), ascii=True, desc="Training | Validation:")
 
     for epoch in progress_bar:
-        train_logs = []
-        val_logs = []
 
         # Training
         model.train()
@@ -100,7 +110,7 @@ def main():
             loss = criterion(predictions, output_scaler(batch.target_vector))
             loss.backward()
 
-            train_logs.append(supervised_pf_logs(loss, output_scaler.inverse(predictions), batch))
+            metrics_train(preds=predictions, target=output_scaler(batch.target_vector), preds_pf=output_scaler.inverse(predictions), batch=batch)
 
             optimizer.step()
 
@@ -112,18 +122,21 @@ def main():
                 batch = batch.to(device)
 
                 predictions = model(batch)
-                loss = criterion(predictions, output_scaler(batch.target_vector))
 
-                val_logs.append(supervised_pf_logs(loss, output_scaler.inverse(predictions), batch))
+                metrics_val(preds=predictions, target=output_scaler(batch.target_vector), preds_pf=output_scaler.inverse(predictions), batch=batch)
 
-        # Logging
-        loss_str, r2score_str, mean_rel_P_error_str, median_rel_P_error_str = logs2str(train_logs, val_logs)
+        overall_metrics_train = nested_to_record(metrics_train.compute(), sep='_')
+        overall_metrics_val = nested_to_record(metrics_val.compute(), sep='_')
 
-        # the PF errors reach a minimum but then start climbing again while the loss keeps declining because the loss focuses on minimizing some larger scale
-        # components of itself while neglecting the smaller ones which has a noticeable effect on the PF.
-        progress_bar.set_description(
-            f"Training | Validation: {loss_str}; {r2score_str}; relative active power error: {mean_rel_P_error_str}, {median_rel_P_error_str}"
-        )
+        description = "Training | Validation:"
+
+        for key in overall_metrics_train.keys():
+            description += f" {key}: ({overall_metrics_train[key]:2.4f} | {overall_metrics_val[key]:2.4f});"
+
+        progress_bar.set_description(description)
+
+        metrics_train.reset()
+        metrics_val.reset()
 
 
 if __name__ == '__main__':

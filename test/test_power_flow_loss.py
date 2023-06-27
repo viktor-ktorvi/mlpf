7d5def528cec7cbd78596b09e249717524e93609
viktor-ktorvi <todosijevicviktor998@gmail.com>
2023-06-27 20:37:49 +0200

    add single topology(case118) but random parameters test

--- test/test_power_flow_loss.py
+++ test/test_power_flow_loss.py
@@ -1,16 +1,20 @@
+import torch
 import unittest
 import warnings
-from typing import Dict
 
 import numpy as np
 import pandapower as pp
-import torch
+import pandapower.networks as pn
+
 from pypower.ppoption import ppoption
 from pypower.runpf import runpf
+from typing import Dict
 
 from mlpf.data.conversion.numpy.power_flow import ppc2power_flow_arrays
 from mlpf.data.conversion.torch.power_flow import ppc2power_flow_tensors
+from mlpf.data.generate.generate_uniform_data import generate_uniform_ppcs
 from mlpf.data.utils.pandapower_networks import get_all_pandapower_networks
+
 from mlpf.loss.numpy import power_flow as pf_numpy
 from mlpf.loss.torch import power_flow as pf_torch
 
@@ -49,7 +53,7 @@ def numpy_get_power_flow_loss(ppc: Dict) -> float:
 class TestPowerFlowLoss(unittest.TestCase):
     def test_many_topologies(self):
         """
-        Test for various grids if the power flow loss function calculates near 0 loss for solved ppcs in float64 accuracy.
+        Test for various grids if the power flow loss function calculates the loss bellow a certain threshold for solved ppcs in float64 accuracy.
         :return:
         """
         warnings.filterwarnings("ignore", message="Casting complex values to real discards the imaginary part")
@@ -66,6 +70,31 @@ class TestPowerFlowLoss(unittest.TestCase):
 
             tolerance = tolerance_VA / ppc["baseMVA"]
 
+            self.assertLess(torch_get_power_flow_loss(ppc), tolerance)
+            self.assertLess(numpy_get_power_flow_loss(ppc), tolerance)
+
+    def test_case118_uniform(self):
+        """
+        Test for single topology but with random grid parameter values if the power flow loss function calculates the loss bellow a certain threshold for solved ppcs in float64 accuracy.
+        :return:
+        """
+        warnings.filterwarnings("ignore", message="Casting complex values to real discards the imaginary part")
+        net = pn.case118()
+        base_ppc = pp.converter.to_ppc(net, init="flat")
+
+        base_ppc, converged = runpf(base_ppc, ppopt=ppoption(OUT_ALL=0, VERBOSE=0))
+
+        ppc_list = generate_uniform_ppcs(
+            base_ppc,
+            how_many=1000,
+            low=0.9,
+            high=1.1
+        )
+
+        tolerance_VA = 1  # P error + Q error on entire grid in Volt-Amps
+        tolerance = tolerance_VA / base_ppc["baseMVA"]
+
+        for ppc in ppc_list:
             self.assertLess(torch_get_power_flow_loss(ppc), tolerance)
             self.assertLess(numpy_get_power_flow_loss(ppc), tolerance)
 

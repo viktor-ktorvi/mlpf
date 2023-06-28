f19bf41b07fb5ffaab527b06b6d503ed7a46a0ea
viktor-ktorvi <todosijevicviktor998@gmail.com>
2023-06-28 21:54:12 +0200

    ppc_runopf

--- mlpf/utils/ppc.py
+++ mlpf/utils/ppc.py
@@ -2,6 +2,7 @@ import copy
 from typing import Dict
 
 from pypower.ppoption import ppoption
+from pypower.runopf import runopf
 from pypower.runpf import runpf
 
 
@@ -13,8 +14,23 @@ def ppc_runpf(ppc: Dict) -> Dict:
     :return: PF solved PPC.
     """
     ppopt = ppoption(OUT_ALL=0, VERBOSE=0)
-    ppc, converged = runpf(copy.deepcopy(ppc), ppopt=ppopt)
+    pf_ppc, converged = runpf(copy.deepcopy(ppc), ppopt=ppopt)
 
     assert converged, "Power flow hasn't converged."
 
-    return ppc
+    return pf_ppc
+
+
+def ppc_runopf(ppc: Dict) -> Dict:
+    """
+    Run an optimal power flow on a PPC.
+
+    :param ppc: PyPower case object.
+    :return: OPF solved PPC.
+    """
+    ppopt = ppoption(OUT_ALL=0, VERBOSE=0)
+    opf_ppc = runopf(ppc, ppopt=ppopt)
+
+    assert opf_ppc['success'], "Optimal power flow hasn't converged."
+
+    return opf_ppc
